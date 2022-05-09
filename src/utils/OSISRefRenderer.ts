196a66507551803e3989928877dd00b6b5fb3c14
Tojo Rakotoarison <tojo.r@protonmail.com>
2022-05-09 10:50:50 +0200

    Move Bible book names out of OSISRefRenderer

--- src/utils/OSISRefRenderer.ts
+++ src/utils/OSISRefRenderer.ts
@@ -2,102 +2,15 @@
 
 import { OSISRef, OSISRefType } from '../models/OSISRef';
 
-const bookTables = {
-    fr: {
-        Gen: 'Genèse',
-        Exod: 'Exode',
-        Lev: 'Lévitique',
-        Num: 'Nombres',
-        Deut: 'Deutéronome',
-        Josh: 'Josué',
-        Judg: 'Juges',
-        Ruth: 'Ruth',
-        '1Sam': '1 Samuel',
-        '2Sam': '2 Samuel',
-        '1Kgs': '1 Rois',
-        '2Kgs': '2 Rois',
-        '1Chr': '1 Chroniques',
-        '2Chr': '2 Chroniques',
-        Ezra: 'Esdras',
-        Neh: 'Néhémie',
-        Esth: 'Esther',
-        Job: 'Job',
-        Ps: 'Psaumes',
-        Prov: 'Proverbes',
-        Eccl: 'Ecclésiaste',
-        Song: 'Cantique des Cantiques',
-        Isa: 'Esaie',
-        Jer: 'Jérémie',
-        Lam: 'Lamentations de Jérémie',
-        Ezek: 'Ezechiel',
-        Dan: 'Daniel',
-        Hos: 'Osée',
-        Joel: 'Joël',
-        Amos: 'Amos',
-        Obad: 'Abdias',
-        Jonah: 'Jonas',
-        Mic: 'Michée',
-        Nah: 'Nahum',
-        Hab: 'Habacuc',
-        Zeph: 'Sophonie',
-        Hag: 'Aggée',
-        Zech: 'Zacharie',
-        Mal: 'Malachie',
-        Bar: 'Baruch',
-        AddDan: 'Ajouts à Daniel',
-        Bel: 'Bel et le Dragon',
-        SgThree: 'Cantique des trois enfants',
-        Sus: 'Suzanne',
-        '1Esd': '1 Esdras',
-        '2Esd': '2 Esdras',
-        AddEsth: 'Esther grec',
-        EpJer: 'Lettre de Jérémie',
-        Jdt: 'Judith',
-        '1Macc': '1 Macchabées',
-        '2Macc': '2 Macchabées',
-        '3Macc': '3 Macchabées',
-        '4Macc': '4 Maccabées',
-        PrMan: 'Manasseh',
-        Sir: 'Siracide',
-        Tob: 'Tobit',
-        Wis: 'Sagesse',
-        Matt: 'Matthieu',
-        Mark: 'Marc',
-        Luke: 'Luc',
-        John: 'Jean',
-        Acts: 'Actes des Apôtres',
-        Rom: 'Romains',
-        '1Cor': '1 Corinthiens',
-        '2Cor': '2 Corinthiens',
-        Gal: 'Galates',
-        Eph: 'Ephésiens',
-        Phil: 'Philippiens',
-        Col: 'Colossiens',
-        '1Thess': '1 Thessaloniciens',
-        '2Thess': '2 Thessaloniciens',
-        '1Tim': '1 Timothée',
-        '2Tim': '2 Timothée',
-        Titus: 'Tite',
-        Phlm: 'Philémon',
-        Heb: 'Hébreux',
-        Jas: 'Jacques',
-        '1Pet': '1 Pierre',
-        '2Pet': '2 Pierre',
-        '1John': '1 Jean',
-        '2John': '2 Jean',
-        '3John': '3 Jean',
-        Jude: 'Jude',
-        Rev: 'Apocalypse',
-    },
-
-    en: {
-        Gen: 'Genesis',
-    },
-};
-
 export class OSISRefRenderer {
+
+    static getDataForLocale(lang: string): any {
+		// TODO Need to do something if the file is not found.
+        return require(`./bible_locales/${lang}.json`);
+    }
+
     static bookNameFromOSISName(lang: string, OSISName: string): string {
-        let hrBookName = bookTables[lang][OSISName];
+        let hrBookName = OSISRefRenderer.getDataForLocale(lang)[OSISName];
         if (hrBookName === undefined) {
             throw new Error(`Unknown bookname ${OSISName} in language ${lang}`);
         }

c6d79319f352dee2149a2c9c143b96d18b04233c
Tojo Rakotoarison <tojo.r@protonmail.com>
2022-06-01 19:11:07 +0200

    Use fully typesafe-i18n for i18n, removed json locale file for OSISRef rendering

--- test/utils/OSISRefRenderer.test.ts
+++ test/utils/OSISRefRenderer.test.ts
@@ -1,49 +1,53 @@
 'use strict';
 const tap = require('tap');
-import { LocalStorageBooknameProvider } from '../../src/utils/LocalStorageBooknameProvider';
 import { OSISRefRenderer } from '../../src/utils/OSISRefRenderer';
 
-tap.test(
-    'OsisRefRenderer render function is working for french.',
-    (t) => {
-        let l = new LocalStorageBooknameProvider('fr');
-        let osisRefRenderer = new OSISRefRenderer(l);
+import { loadLocale } from '../../src/i18n/i18n-util.sync';
+import { i18nObject } from '../../src/i18n/i18n-util';
 
+tap.test('OsisRefRenderer render function is working for french.', (t) => {
+    loadLocale('fr');
+    var LL = i18nObject('fr');
+    let osisRefRenderer = new OSISRefRenderer({
+        getBookName: (bookName) => {
+            return LL[bookName]();
+        },
+    });
+
+    t.equal(
+        osisRefRenderer.render('Matt.2.2'),
+        'Matthieu 2,2',
+        'Working for string book name chapter and verse',
+    ),
         t.equal(
-            osisRefRenderer.render('Matt.2.2'),
-            'Matthieu 2,2',
-            'Working for string book name chapter and verse',
+            osisRefRenderer.render('Matt.2'),
+            'Matthieu 2',
+            'Working for string bookname chapter',
         ),
-            t.equal(
-                osisRefRenderer.render('Matt.2'),
-                'Matthieu 2',
-                'Working for string bookname chapter',
-            ),
-            t.equal(
-                osisRefRenderer.render('Matt'),
-                'Matthieu',
-                'Working for book only',
-            ),
-            t.equal(
-                osisRefRenderer.render('1Chr.12.15'),
-                '1 Chroniques 12,15',
-                'Working for bookName with number and chapter, verse',
-            ),
-            t.equal(
-                osisRefRenderer.render('1Chr.12'),
-                '1 Chroniques 12',
-                'Working for bookName with number and chapter',
-            ),
-            t.equal(
-                osisRefRenderer.render('1Chr'),
-                '1 Chroniques',
-                'Working for bookName with number only',
-            ),
-            t.equal(
-                osisRefRenderer.render('Acts.1.5-1Cor.12.13'),
-                'Actes des Apôtres 1,5-1 Corinthiens 12,13',
-                'Working on book ranges',
-            ),
-            t.end();
-    },
-);
+        t.equal(
+            osisRefRenderer.render('Matt'),
+            'Matthieu',
+            'Working for book only',
+        ),
+        t.equal(
+            osisRefRenderer.render('1Chr.12.15'),
+            '1 Chroniques 12,15',
+            'Working for bookName with number and chapter, verse',
+        ),
+        t.equal(
+            osisRefRenderer.render('1Chr.12'),
+            '1 Chroniques 12',
+            'Working for bookName with number and chapter',
+        ),
+        t.equal(
+            osisRefRenderer.render('1Chr'),
+            '1 Chroniques',
+            'Working for bookName with number only',
+        ),
+        t.equal(
+            osisRefRenderer.render('Acts.1.5-1Cor.12.13'),
+            'Actes des Apôtres 1,5-1 Corinthiens 12,13',
+            'Working on book ranges',
+        ),
+        t.end();
+});
